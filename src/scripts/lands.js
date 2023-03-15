8f50ab84a47e2896cd11d5077a8c9dffd0be3eb4
macbeamish <macbeamish@gmail.com>
2023-03-15 23:32:52 +1300

    land feed fixed

--- src/scripts/lands.js
+++ src/scripts/lands.js
@@ -1,46 +1,59 @@
 console.log("lands.js loaded");
-function terraForm() {
-    for (let i = 0; i <= 5; i++) {
-        console.log(`continent${i} building`)
-        populateContinent(i);
-    }
-}
-
-function populateContinent(cont) {
-const continent = document.querySelector(`#continent${cont}`);
-for(let k= 1; k < 10; k++){
-    var nextLand = getRandomInt(1,7);
-    console.log(`land${nextLand} at position ${k} added to continent${cont}`)
-    landBuild(nextLand, cont);   
-}
-if (cont == 0 ){
-continent.setAttribute('position', {x: -10, y: 0.1, z: -5});
-}
-else{
-    cont = cont * 10;
-    continent.setAttribute('position', {x: cont, y: 0.1, z: -5});
-}
-}
-
-function landBuild(id, cont){  
-    var conti = document.querySelector(`#continent${cont}`);
-    if(id!=7 && id!=6){    
-    location = (location * 4 + 1);
-    }
-    else {location = location * 4 + 5;}
-    const land = document.createElement('a-entity');
-    
-    land.setAttribute('gltf-model', `#${id}`);
-    land.setAttribute('position', {x: location, y: 0.1, z: -3});
-    land.setAttribute('ammo-body', {type: 'kinematic', emitCollisionEvents: true});
-    land.setAttribute('ammo-shape', {type: 'box', fit: 'manual', halfExtents: {x: 1, y: 1, z: 1}, offset: {x: 0, y: 1, z: 0}});
-    land.setAttribute('activationState','active')
-    conti.appendChild(land);
-    console.log(`land${id} added`);
-}
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function terraForm() {   
+var previousland = 1;
+var landnum = 1
+var nextXLocation = 0
+    for(let k = 1; k < 101; k++){
+        while(landnum == previousland)
+        {landnum = getRandomInt(1,7); console.log("landID changed loop");}
+      
+        switch(landnum){
+            case 1: landsize = 8; break;
+            case 2: landsize = 8; break;
+            case 3: landsize = 10; break;
+            case 4: landsize = 10; break;
+            case 5: landsize = 10; break;
+            case 6: landsize = 14; break;
+            case 7: landsize = 14; break;
+        }
+        nextXLocation += (landsize/2 + 5);
+        landBuild(k, landnum, nextXLocation)
+        previousland = landnum;
+    }
+   
+}
+
+
+function landBuild(id, landnumber, x){  
+const game = document.querySelector('#game');
+var halfextents = "";
+switch(landnumber){
+    case 1:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 2:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 3:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 4:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 5:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 6:halfextents = "x: 2, y: 2, z: 2"; break;
+    case 7:halfextents = "x: 2, y: 2, z: 2"; break;
+}
+const land = document.createElement('a-entity');
+land.setAttribute('id', `land${id}`);
+land.setAttribute('gltf-model', `#${landnumber}`);
+land.setAttribute('position', {x: x, y: 0.1, z: -10});
+land.setAttribute('ammo-body', {type: 'kinematic', emitCollisionEvents: true});
+land.setAttribute('ammo-shape', {type: 'box', fit: 'manual', halfExtents: `${halfextents}`, offset: {x: 0, y: 1, z: 0}});
+land.setAttribute('activationState','active')
+game.appendChild(land);
+console.log(`land${id} added with land ${landnumber} at location x = ${x}`);
+
+}
 terraForm();
+
+
+ 
