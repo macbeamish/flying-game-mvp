334031c24e9e94bff7a5b20b91d1e406a866d07b
macbeamish <macbeamish@gmail.com>
2023-03-27 00:31:35 +1300

    ,

--- src/scripts/lands.js
+++ src/scripts/lands.js
@@ -1,34 +1,147 @@
 // todo, add a function to check the scoreboard and update the speed variable at differnet scores.
-// todo fix the double ups on lands loaded in land-tick.
-
+// todo, add a function to check for collision with the sprite.
+// todo add function to land-tick that increases speed of lands if score > x 
+// add attributes to lands that set their boundaries and balloon boundaries
 const numberOfLands = 51;
+
 AFRAME.registerComponent('land-tick', {
+    // Initialize the component
     init() {
 
     },
+
+    // Update the component on every animation frame
     tick(time, timeDelta){
 
-        // console.log('checking land position');
-            var game = document.querySelector('#game');         
+        // Select the "game" HTML element
+        var game = document.querySelector('#game');
+        
+        // Get the world position of the current entity
+        var worldPosition = new THREE.Vector3();          
+        this.el.object3D.getWorldPosition(worldPosition);
+
+        // Extract the x position from the world position vector
+        var landX = worldPosition.x;
+        var lSpeed = this.el.getAttribute("land-speed");
+
+        // If the entity is off the left side of the screen
+        if (landX < -50){
+
+            // Log a message to the console
+            // console.log(`${this.el.id} is off screen`);
+            // if(document.querySelector('scoreboard').getAttribute('score') != null){
+            //     lSpeed = 0.1;
+            
+            //     if(document.querySelector('scoreboard').getAttribute('score') > 100){
+            //         lSpeed = 0.12;
+            //     }
+            // }
+            // else{
+            //     console.log ("scoreboard is null");
+            //     lSpeed = 0.1;
+            // }
+            // Set a new position for the entity
+            var newLandPosition = 460;
+
+            // Get the "landnum" attribute of the entity
+            var landnum = this.el.getAttribute('landnum');     
+
+            // Call a function to replace the entity with a new one
+            landBuild(this.el.id, landnum, newLandPosition, lSpeed);
+
+            // Remove the entity from the game
+            game.removeChild(this.el);  
+        }
+    }  
+});
+
+//tune these values to be more accurate
+AFRAME.registerComponent('land-collide', {
+    init() {
+
+    },
+    tick(time, timeDelta){     
+            //Get land position and size
             var worldPosition = new THREE.Vector3();          
             this.el.object3D.getWorldPosition(worldPosition);
-            var landx = worldPosition.x;
-            if (landx < -50){
-                console.log(`${this.el.id} is off screen`);  
-                var newLandPosition = 460;  
-                var  landnum = this.el.getAttribute('landnum');     
-                landBuild(this.el.id, landnum, newLandPosition);
-                game.removeChild(this.el);
+            var landHeight = this.el.getAttribute('land-height');
+            var landWidth = this.el.getAttribute('land-width');
+            var landX = worldPosition.x;
+            var landY = worldPosition.y;
+            //get baloon altitude and size
+            var baloonAltitude = this.el.getAttribute('baloon-altitude');
+            var baloonWidth = this.el.getAttribute('baloon-width');
+
+            //Get sprite position and size
+            var sprite = document.querySelector('#sprite');
+            var spritePosition = new THREE.Vector3();
+            sprite.object3D.getWorldPosition(spritePosition);
+            var spriteX = spritePosition.x;
+            var spriteY = spritePosition.y;
+            var spriteHeight = sprite.getAttribute("sprite-height");
+            var spriteWidth = sprite.getAttribute("sprite-width");
+
+            //Check for land collision
+            if (spriteX + (spriteWidth/2) > landX - (landWidth/2) && spriteX - (spriteWidth/2) < landX + (landWidth/2) && spriteY < landY + landHeight){
+                console.log("land collision");
+                // pause();
+                
+          
+            }
+            if(spriteY + spriteHeight > baloonAltitude && spriteX + (spriteWidth/2) > landX - (baloonWidth/2) && spriteX - (spriteWidth/2) < landX + (baloonWidth/2)){
+                console.log("balloon collision");
+                // pause();
                 
             }
+
+
+      
 
     }
   
 });
-function landomizer(num){
+
+AFRAME.registerComponent('land-move', {
+
+    // Initialize the component
+    init: function () {
+        
+    },
+
+    // Update the component on every animation frame
+    tick: function (time, delta) { 
+    
+        // Set direction of the entity
+        const direction = new THREE.Vector3(-51, 0, 0); 
+        var speed = this.el.getAttribute('land-speed');
+        this.el.velocity = new THREE.Vector3();
+        this.el.velocity.addScaledVector(direction, speed);
+
+        // Get the current position of the entity
+        const position = this.el.object3D.position.clone();
+
+        // Calculate the time since the last animation frame
+        const dt = delta / 1000;
+
+        // Update the position of the entity based on its velocity
+        position.addScaledVector(this.el.velocity, dt);
+
+        // Set the new position of the entity
+        this.el.object3D.position.copy(position);
+    }
+});
+
+
+  function landomizer(num){
+
+    // Generate a random integer between 1 and 7 (inclusive) 
     landnum = getRandomInt(1,7);
     while(landnum == num)
-    {landnum = getRandomInt(1,7); console.log("landID changed loop");}
+    {
+        // Generate a new random integer for 'landnum'
+        landnum = getRandomInt(1,7);
+        // console.log("rerolled for double up");
+    }
     return landnum;
 }
 
@@ -41,7 +154,7 @@ function getRandomInt(min, max) {
 }
 
 function terraForm() {   
-
+var lSpeed = 0.1;
 var landnumber = 1
 var nextXLocation = 0
     for(let k = 1; k < numberOfLands; k++){
@@ -56,42 +169,50 @@ var nextXLocation = 0
             case 7: landsize = 14; break;
         }
         nextXLocation += (landsize/2 + 5);
-        landBuild(k, landnumber, nextXLocation)
+        landBuild(k, landnumber, nextXLocation, lSpeed)
        
     }
    
 }
 
 
-function landBuild(id, landnumber, x){  
-const game = document.querySelector('#game');
-var halfextents = "";
-switch(landnumber){
-    case 1:halfextents = "x: 4, y: 2, z: 4"; break;
-    case 2:halfextents = "x: 4, y: 2, z: 4"; break;
-    case 3:halfextents = "x: 6, y: 4, z: 6"; break;
-    case 4:halfextents = "x: 8, y: 8, z: 8"; break;
-    case 5:halfextents = "x: 8, y: 8, z: 8"; break;
-    case 6:halfextents = "x: 10, y: 10, z: 10"; break;
-    case 7:halfextents = "x: 10, y: 10, z: 10"; break;
+function landBuild(id, landnumber, x, landSpeed){  
+    var landHeight = 0;
+    var landWidth = 0;
+    switch(landnumber){
+        case 1: landWidth = 4; landHeight = 5; break;
+        case 2: landWidth = 4; landHeight = 5; break;
+        case 3: landWidth = 6; landHeight = 8; break;
+        case 4: landWidth = 6; landHeight = 8; break;
+        case 5: landWidth = 6; landHeight = 9; break;
+        case 6: landWidth = 8; landHeight = 10; break;
+        case 7: landWidth = 8; landHeight = 10; break;
+    }
+if(landnumber==1 || landnumber==2){
+    baloonAltitude = 50;
+    baloonWidth = 4
 }
-var dir = new THREE.Vector3(-51,0,0);
-var speed = 0.1;
+else{
+    baloonWidth = 0;
+    baloonAltitude = 100;
+} 
+   
+const game = document.querySelector('#game');
 const land = document.createElement('a-entity');
-const velocity = dir.clone().multiplyScalar(speed);
-land.setAttribute('velocity', velocity);
 land.setAttribute('id', `${id}`);
 land.setAttribute('landnum', landnumber);
-land.setAttribute('gltf-model', `#${landnumber}`);
+land.setAttribute('gltf-model', `url(./src/assets/3dmodels/Lands/FlyingLand_${landnumber}.glb)#model`);
 land.setAttribute('position', {x: x, y: 0.1, z: -15});
-land.setAttribute('ammo-body', {type: 'kinematic', emitCollisionEvents: true});
-land.setAttribute('ammo-shape', {type: 'box', fit: 'manual', halfExtents: `${halfextents}`, offset: {x: 0, y: 1, z: 0}});
-land.setAttribute('activationState','active')
-// land.setAttribute('animation',{property: 'position', to: '-999 0 0', loop: false, dur: 100000});
 land.setAttribute('land-tick', {});
+land.setAttribute('land-speed', landSpeed);
+land.setAttribute('land-move', {});
+land.setAttribute('land-collide', {});
+land.setAttribute('land-height', landHeight);
+land.setAttribute('land-width', landWidth);  
+land.setAttribute('baloon-altitude', baloonAltitude);
+land.setAttribute('baloon-width', baloonWidth);
 game.appendChild(land);
-console.log(`land${id} added with land ${landnumber} at location x = ${x}`);
-
+// console.log(`land${id} added with land ${landnumber} at location x = ${x}`);
 }
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
+//todo add bonuses
+//todo add scoreboard
+//todo add UI Overlays
+//todo add screens
+// figure out figma
+
 console.log("main.js loaded");
 document.addEventListener("DOMContentLoaded", function(){
   const sceneEl = document.querySelector('a-scene');
@@ -9,7 +15,106 @@ document.addEventListener("DOMContentLoaded", function(){
 	});
   });
 
+
+function pause() {
+	console.log("Pause button clicked");
+	const entities = document.querySelectorAll('[land-move]');
+	entities.forEach(entity => {
+		entity.setAttribute('land-speed', 0)
+	});
+	  const sprite = document.querySelector("#sprite");
+	  var oldSpeed = sprite.getAttribute('sprite-speed');
+	  sprite.setAttribute('sprite-speed', 0);
+	  sprite.setAttribute('old-speed', oldSpeed);
+ 
+
+}
+function resume() {
+	console.log('resume button clicked');
+	const entities = document.querySelectorAll('[land-move]');
+	entities.forEach(entity => {
+		entity.setAttribute('land-speed', 0.1)
+	});
+	const sprite = document.querySelector("#sprite");
+	var oldSpeed = sprite.getAttribute('old-speed');
+	sprite.setAttribute('sprite-speed', oldSpeed);
+
+}
   
+
+
+  function loadPage(url, divId) {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+	  xmlhttp = new XMLHttpRequest();
+	} else {
+	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+		document.getElementById(divId).innerHTML = this.responseText;
+	  }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+  }
+
+function gameOverPause(){
+	var sprite
+}
+
+function gamePause(){
+	
+}
+
+function unPause(){
+sprite
+}
+
+//   AFRAME.registerComponent('pause-velocity', {
+// 	init: function () {
+// 	  // Add click event listener to the entity
+// 	  this.el.addEventListener('click', this.pauseVelocity.bind(this));
+// 	},
+  
+// 	pauseVelocity: function () {
+// 	  // Get all entities in the scene
+// 	  const entities = document.querySelectorAll('[velocity]');
+  
+// 	  // Loop through each entity and set its velocity to 0
+// 	  entities.forEach(entity => {
+// 		const velocity = entity.getAttribute('velocity');
+// 		entity.setAttribute('velocity', '0 0 0');
+// 	  });
+// 	}
+//   });
+
+//   AFRAME.registerComponent('collision-check', {
+// 	init: function () {
+	
+// 	},
+  
+// 	tick: function () {
+// 	  // Get the position and scale of each entity
+// 	  const entity1Pos = this.entity1.object3D.position;
+// 	  const entity1Scale = this.entity1.object3D.scale;
+// 	  const entity2Pos = this.entity2.object3D.position;
+// 	  const entity2Scale = this.entity2.object3D.scale;
+  
+// 	  // Calculate the distance between the two entities
+// 	  const dx = entity1Pos.x - entity2Pos.x;
+// 	  const dy = entity1Pos.y - entity2Pos.y;
+// 	  const dz = entity1Pos.z - entity2Pos.z;
+// 	  const distance = Math.sqrt(dx*dx + dy*dy + dz*dz);
+  
+// 	  // Check if the entities are colliding
+// 	  if (distance < (entity1Scale.x + entity2Scale.x) / 2) {
+// 		console.log('Entities are colliding!');
+// 		// Emit a custom event indicating a collision has occurred
+// 		this.el.emit('collision', { entity1: this.entity1, entity2: this.entity2 });
+// 	  }
+// 	}
+//   });
   // arReady event triggered when ready
 
 
@@ -26,3 +131,14 @@ document.addEventListener("DOMContentLoaded", function(){
 	//   console.log("plane click");
 	// });
 
+// if player.left > land.right
+//  return false; not overlapping
+//  endif
+ 
+//  if player.right < land.left
+//   return false; not overlapping
+//  endif
+ 
+//  ;Is overlapping
+//  return true
+//  AABB -> axis aligned bounding box
